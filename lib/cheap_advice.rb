b9bd1c7559612e61ca99c566a636d063a0ac59c6
Kurt Stephens <ks.github@kurtstephens.com>
2011-01-14 16:22:44 -0600

    Initial working version.

---
+++ lib/cheap_advice.rb
@@ -0,0 +1,188 @@
+
+# Provides cheap advice mechanism for Ruby.
+# kurt dot ruby at kurtstephens dot com
+#
+class CheapAdvice
+  attr_accessor :before, :after, :around, :advised
+
+  NULL_PROC = lambda { | ar | }
+  NULL_AROUND_PROC = lambda { | ar, result | result.call }
+  EMPTY_HASH = { }.freeze
+
+  # options:
+  #   :before
+  #   :after
+  #   :around
+  def initialize opts, &blk
+    @advised = [ ]
+
+    opts_hash = EMPTY_HASH
+    opts_key = nil
+    
+    case opts
+      # advice :before => lambda ...
+    when Hash 
+      opts_hash = opts
+      # advice :method, :before do ... end
+    when Symbol 
+      opts_key = opts
+    end
+
+    @before = (opts_key == :before ? blk : opts_hash[:before]) || 
+      NULL_PROC
+    @after  = (opts_key == :after  ? blk : opts_hash[:after])  || 
+      NULL_PROC
+    @around = (opts_key == :around ? blk : opts_hash[:around]) || 
+      NULL_AROUND_PROC
+
+    @blk = blk
+  end
+
+
+  # Apply advice to class and method.
+  def advise! cls, method, opts = nil
+    return cls.map { | x | advise! x, method, opts } if 
+      cls.kind_of?(Enumerable)
+    return method.map { | x | advise! cls, x, opts } if 
+      method.kind_of?(Enumerable)
+
+    advice = self
+
+    advised = Advised.new(advice, cls, method, opts)
+
+    advised.apply_advice_methods!
+
+    cls.class_eval do
+      define_method advised.new_method do | *args |
+        ar = ActivationRecord.new(advised, self, args)
+        
+        do_result = Proc.new do
+          self.send(advised.before_method, ar)
+          begin
+            ar.result = self.send(advised.old_method, *ar.args)
+          rescue Exception => err
+            ar.error = err
+          ensure
+            self.send(advised.after_method, ar)
+          end
+
+          ar.result
+        end
+        
+        self.send advised.around_method, ar, do_result
+        
+        raise ar.error if ar.error
+        
+        ar.result
+      end
+    end
+    
+    advised.advise!
+
+    @advised << advised
+
+    advised
+  end
+
+
+  def unadvise!
+    @advised.each { | x | x.unadvise! }
+  end
+
+
+  def readvise!
+    @advised.each { | x | x.advise! }
+  end
+
+
+  # Represents the application of advice to a class and method.
+  class Advised
+
+    @@advice_id ||= 0
+
+    attr_reader :advice, :cls, :method, :options
+    attr_reader :advice_id
+    attr_reader :old_method, :new_method
+    attr_reader :before_method, :after_method, :around_method
+
+    def initialize *args
+      @advice, @cls, @method, @options = *args
+
+      @options ||= { }
+
+      @advice_id = @@advice_id += 1
+      
+      @old_method = "__advice_old_#{@@advice_id}_#{@method}"
+      @new_method = "__advice_new_#{@@advice_id}_#{@method}"
+    
+      @before_method = "__advice_before_#{@@advice_id}_#{@method}"
+      @after_method  = "__advice_after_#{@@advice_id}_#{@method}"
+      @around_method = "__advice_around_#{@@advice_id}_#{@method}"
+    end
+
+
+    def [](k)
+      @options[k]
+    end
+
+
+    def []=(k, v)
+      @options[k] = v
+    end
+
+
+    def apply_advice_methods!
+      this = self
+      @cls.instance_eval do 
+        define_method(this.before_method, &this.advice.before)
+        define_method(this.after_method,  &this.advice.after)
+        define_method(this.around_method, &this.advice.around)  
+      end
+    end
+
+
+    def advise!
+      this = self
+      @cls.instance_eval do
+        alias_method this.old_method, this.method if 
+          method_defined? this.method and 
+          ! method_defined? this.old_method
+
+        alias_method this.method, this.new_method
+      end
+    end
+
+    def unadvise!
+      this = self
+      @cls.instance_eval do
+        alias_method this.method, this.old_method if 
+          method_defined? this.old_method
+      end
+    end
+  end
+
+
+  # Represents the activation record of a method invocation.
+  class ActivationRecord
+    attr_reader :advised, :rcvr, :args
+    attr_accessor :result, :error, :body
+    
+    def initialize *args
+      @advised, @rcvr, @args = *args
+    end
+
+    def advice
+      @advised.advice
+    end
+
+    def method
+      @advised.method
+    end
+
+    def caller(offset = 0)
+      ::Kernel.caller(offset + 2)
+    end
+  end
+
+end
+
--- spec/cheap_advice_spec.rb
+++ spec/cheap_advice_spec.rb
@@ -1,7 +1,90 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "CheapAdvice" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+class CheapAdvice
+  module Test
+
+    class Foo
+      attr_accessor :foo
+      attr_reader :_baz
+      
+      def baz(arg)
+        @_baz = 5 + arg
+      end
+
+      def do_it(arg)
+        yield arg + 7
+      end
+    end
+    
+    
+    class Bar
+      attr_accessor :bar
+      attr_reader :_baz
+      
+      def baz(arg)
+        @_baz = 7 + arg
+      end
+    end
   end
 end
+
+
+describe "CheapAdvice" do
+  attr_reader :tracing_advice
+  attr_reader :f, :b
+
+  before(:each) do
+    @tracing_advice = CheapAdvice.new(:around) do | ar, body |
+      ar.advice.log "  TRACE: before #{ar.rcvr.class}\##{ar.method}(#{ar.args.join(", ")})"
+      ar.advice.log "         foo = #{@foo.inspect}"
+      ar.advice.log "         bar = #{@bar.inspect}"
+      result = body.call
+      ar.advice.log "  TRACE: after  #{ar.rcvr.class}\##{ar.method}(#{ar.args.join(", ")}) => #{result.inspect}"
+      ar.result = "yo!"
+      ar.advice.log "  TRACE: return #{ar.result.inspect}"
+      "oy!" # Not relevant.
+    end
+    @tracing_advice.instance_eval do
+      def log msg = nil
+        return @log unless msg
+        (@log ||= [ ]) << msg.dup
+      end
+    end
+  end
+
+  it "handles simple tracing_advice example." do
+    @f = CheapAdvice::Test::Foo.new
+    @b = CheapAdvice::Test::Bar.new
+
+    assert_without_advice
+    tracing_advice.log.should == nil
+
+    tracing_advice.advise!( [CheapAdvice::Test::Foo, CheapAdvice::Test::Bar], [ :bar, :bar=, :baz ])
+    f.foo = 10
+    f.foo.should == 10
+    f.baz(10).should == "yo!"
+    b.bar = 101
+    b.bar.should == "yo!"
+    b.baz(10).should == "yo!"
+    tracing_advice.log.should_not == nil
+    tracing_advice.log.size.should == 20
+    
+    tracing_advice.unadvise!
+    assert_without_advice
+  end
+
+  def assert_without_advice
+    f.foo = 10
+    f.foo.should == 10
+    f.baz(10).should == 15
+    f._baz.should == 15
+    
+    b.bar = 101
+    b.bar.should == 101
+    b.baz(10).should == 17
+    b._baz.should == 17
+  end
+
+end
+
+

e0f3fcaf060f4e80f16577b159e901f470a92c54
Kurt Stephens <ks.github@kurtstephens.com>
2011-01-17 17:09:06 -0600

    Added example.

---
+++ example/ex01.rb
@@ -0,0 +1,40 @@
+$: << File.expand_path('../../lib', __FILE__)
+
+require 'cheap_advice'
+require 'time'
+
+ 
+class MyClass
+  def foo
+    42
+  end
+end
+
+class MyOtherClass
+  def bar
+    43
+  end
+end
+
+a = MyClass.new
+b = MyOtherClass.new
+
+trace_advice = CheapAdvice.new(:around) do | ar, body |
+  ar.advice[:log].puts "#{Time.now.iso8601(6)} #{ar.rcvr.class} #{ar.method} #{ar.rcvr.object_id}"
+  body.call
+  ar.advice[:log].puts "#{Time.now.iso8601(6)} #{ar.rcvr.class} #{ar.method} #{ar.rcvr.object_id} => #{ar.result.inspect}"    
+end
+trace_advice[:log] = File.open("trace.log", "a+")
+trace_advice.advise!(MyClass, :foo)
+trace_advice.advise!(MyOtherClass, :bar)
+
+
+a.foo
+b.bar
+
+trace_advice.disable!
+
+a.foo
+b.bar
+
+

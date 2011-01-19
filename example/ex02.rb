0fb3044e2f058b1afcf8af5710d96198ff928bbe
Kurt Stephens <ks.github@kurtstephens.com>
2011-01-18 19:10:42 -0600

    Add basic Benchmark.

--- example/ex02.rb
+++ example/ex02.rb
@@ -4,6 +4,7 @@ require 'cheap_advice'
 require 'cheap_advice/configuration'
 require 'cheap_advice/trace'
 require 'yaml'
+require 'benchmark'
 
 require 'rubygems'
 gem 'ruby-debug'
@@ -21,45 +22,58 @@ class MyClass
   end
 end
 
+trace_advice = nil
+trace_config = nil
+
+Benchmark.bm(40) do | bm |
+
 ##################################################
 # Advice
 
-trace_advice = CheapAdvice::Trace.new 
-
-# Configure Trace loggers by name.
-trace_advice.logger[:default] =
-  File.open(File.expand_path("../ex02-default.log", __FILE__), "w")
-
-trace_advice.logger[:alternate] = 
-  File.open(File.expand_path("../ex02-alternate.log", __FILE__), "w")
-
+  bm.report("trace_advice setup") do
+    trace_advice = CheapAdvice::Trace.new 
+    
+    # Configure Trace loggers by name.
+    trace_advice.logger[:default] =
+      File.open(File.expand_path("../ex02-default.log", __FILE__), "w")
+    
+    trace_advice.logger[:alternate] = 
+      File.open(File.expand_path("../ex02-alternate.log", __FILE__), "w")
+  end
 
 ##################################################
 # Advice Configuration
 
 
-# Register Trace advice with configuration.
-trace_config = CheapAdvice::Configuration.new
-trace_config.advice[:trace] = trace_advice
+  bm.report("trace_config setup") do
+    # Register Trace advice with configuration.
+    trace_config = CheapAdvice::Configuration.new
+    trace_config.advice[:trace] = trace_advice
+  end
 
-config_yml = File.expand_path("../ex02-trace-*.yml", __FILE__)
-
-Dir[config_yml].sort.each do | config_yml |
-  # Configure.
-  config_hash = YAML.load_file(config_yml)
-  trace_config.config = config_hash[:advice]
-  trace_config.configure!
-
-  msg = "Using #{config_yml}"
-  puts msg
-  trace_advice.log_all(msg)
-
-  ##################################################
-  # Target activity
+  config_yml = File.expand_path("../ex02-trace-*.yml", __FILE__)
   
-  a = MyClass.new
-  a.foo
-  a.bar
+  Dir[config_yml].sort.each do | config_yml |
+    # Configure.
+    config_hash = YAML.load_file(config_yml)
+    trace_config.config = config_hash[:advice]
+
+    bm.report("configure! using #{File.basename(config_yml)}") do
+      trace_config.configure!
+    end
+
+    msg = "Using #{config_yml}"
+    # puts msg
+    trace_advice.log_all(msg)
+    
+    ##################################################
+    # Target activity
+    
+    a = MyClass.new
+    a.foo
+    a.bar
+  end
 end
+
 
 
